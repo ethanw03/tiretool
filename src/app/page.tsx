9ccd1ee1f4ac2af3ba1f1c2f62ba8e039efa301f
ethanw03 <ethanwong98@yahoo.ca>
2024-03-06 01:35:30 -0800

    dadads

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,40 @@
-'use client';
+'use client'
 
 import React, { useEffect, useState } from 'react';
-import { TireSize, calculateEquivalentSizes } from './util/calculateEquivalentSizes'; // Adjust the path as necessary
+import { TireSize, calculateEquivalentSizes } from './util/calculateEquivalentSizes';
 
 import Image from 'next/image';
-import ResultsDisplay from './components/ResultsDisplay'; // Adjust the path as necessary
-import TireSizeSelector from './components/TireSizeSelector'; // Adjust the path as necessary
-import tireLogo from '../../public/tireLogo.svg'; // Ensure the path matches your project structure
+import ResultsDisplay from './components/ResultsDisplay';
+import TireSizeSelector from './components/TireSizeSelector';
+import tireLogo from '../../public/tireLogo.svg';
 
 const HomePage: React.FC = () => {
   const [selectedSize, setSelectedSize] = useState<{ rimSize?: number; width?: number; aspectRatio?: number } | null>(null);
   const [results, setResults] = useState<TireSize[]>([]);
 
- useEffect(() => {
-  // Check if all three values (rimSize, width, aspectRatio) have been selected
-  if (selectedSize && selectedSize.rimSize !== undefined && selectedSize.width !== undefined && selectedSize.aspectRatio !== undefined) {
-    // Since selectedSize's properties are confirmed to be numbers, TypeScript should not complain here
-    const equivalents = calculateEquivalentSizes(selectedSize as TireSize);
-    setResults(equivalents);
-  }
-}, [selectedSize]); // This effect depends on selectedSize
+  useEffect(() => {
+    // Check if all three values (rimSize, width, aspectRatio) have been selected
+    if (selectedSize && selectedSize.rimSize !== undefined && selectedSize.width !== undefined && selectedSize.aspectRatio !== undefined) {
+      // Since selectedSize's properties are confirmed to be numbers, TypeScript should not complain here
+      const equivalents = calculateEquivalentSizes(selectedSize as TireSize);
+      setResults(equivalents);
+    }
+  }, [selectedSize]); // This effect depends on selectedSize
 
   // Debugging line to test calculateEquivalentSizes function with known inputs
   useEffect(() => {
     console.log("Equivalent Sizes for 15, 205, 55:", calculateEquivalentSizes({ rimSize: 15, width: 205, aspectRatio: 55 }));
   }, []);
+
+  // Logging selectedSize to ensure it's being passed correctly
+  useEffect(() => {
+    console.log("Selected tire size:", selectedSize);
+  }, [selectedSize]);
+
+  // Logging results to ensure they're being passed correctly
+  useEffect(() => {
+    console.log("Results:", results);
+  }, [results]);
 
   return (
     <div className='bg-wheat h-screen w-screen font-sans'>
